f0ca9cdbb6e1be2b7ce9fbc48ba809ba8e90323e
praveenkrishnamoorthy <praveenkrishang@gmail.com>
2023-04-12 09:32:47 +0100

    facebook and google authentication

--- app/src/main/java/com/example/cocktailworld/data/remote/ApiDetails.kt
+++ app/src/main/java/com/example/cocktailworld/data/remote/ApiDetails.kt
@@ -4,4 +4,6 @@ object ApiDetails {
     const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
     const val ALCOHOLIC_URL ="filter.php?a=Alcoholic"
     const val NON_ALCOHOLIC_URL ="filter.php?a=Non_Alcoholic"
+    const val ALL_DRINKS_URL ="search.php"
+    const val LOOK_UP_URL ="lookup.php"
 }
